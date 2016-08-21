d5baf44e5bb4253c7e92a012dd72779a0bd1e8e2
karthick-manoharan <smk26888@gmail.com>
2016-08-21 20:04:36 +0530

    First commit

---
+++ directives/gitProfile.js
@@ -0,0 +1,41 @@
+(function(){
+	'use strict';
+	angular
+		.module('apttus',[])
+		.constant('directiveConfig',{
+			"githubAPI":"https://api.github.com/users/"
+		})
+		.directive('gitProfile',['$http','directiveConfig',showProfile]);
+
+	function showProfile ($http,directiveConfig){
+		var directiveObj = {
+			restrict:'E',
+			replace:true,
+			scope:{
+				username:'@'
+			},
+			templateUrl:'templates/git-profile.html',
+			controller:function getData ($scope){
+				$http.get(directiveConfig.githubAPI + $scope.username)
+					.then(function success(response){
+						if(response.status == 200){
+							$scope.success = true;
+							var data = response.data;
+							$scope.avatar = data.avatar_url;
+							$scope.name = data.name;
+							$scope.followers = data.followers;
+							$scope.following = data.following;
+						}
+						else{
+							$scope.success = false;
+							$scope.message = response.statusText;
+						}
+					},function error(err){
+						$scope.success = false;
+						$scope.message = err.statusText;
+					});
+			}
+		} 
+		return directiveObj;
+	};
+})();
